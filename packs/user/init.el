03ab5959a859866c93d4b2de71f33f3cee5dc38f
Nevill <nevill.dutt@gmail.com>
2014-04-12 22:15:13 +0800

    Support with Chinese

--- packs/user/init.el
+++ packs/user/init.el
@@ -1,18 +1,26 @@
-(setq user-full-name "Nevill Dutt")
+(setq user-full-name "Nevill")
 (setq user-mail-address "nevill.dutt@gmail.com")
 
-;;; change this according to your needs
-;;; you can get "Source Code Pro" from http://sourceforge.net/projects/sourcecodepro.adobe/
-;;; it's a great coding font
+;; change this according to your needs
+;; you can get "Source Code Pro" from http://sourceforge.net/projects/sourcecodepro.adobe/
+;; it's a great coding font
 ;;(defvar default-font "Source Code Pro Regular-13" "My default Emacs font.") ;; it doesn't support Chinese well
-(defvar default-font "Monaco Regular-13" "My default Emacs font.")
+;;(defvar default-font "Monaco Regular-13" "My default Emacs font.")
+;;(set-frame-font default-font nil t)
 
-(set-frame-font default-font nil t)
+; to support chinese characters
+(when (display-graphic-p)
+  (setq fonts
+        (cond ((eq system-type 'darwin)     '("Monaco"   "STHeiti")) ; if STHeiti is not supported, try with: Hiragino Sans GB
+              ((eq system-type 'gnu/linux)  '("Menlo"    "WenQuanYi Zen Hei"))
+              ((eq system-type 'windows-nt) '("Consolas" "Microsoft Yahei"))))
 
-;;; Always have cursor above a number of lines from bottom
+  (setq face-font-rescale-alist '(("STHeiti" . 1.1) ("Microsoft Yahei" . 1.2) ("WenQuanYi Zen Hei" . 1.2)))
+  (set-face-attribute 'default nil :font
+                      (format "%s:pixelsize=%d" (car fonts) 13))
+  (dolist (charset '(kana han symbol cjk-misc bopomofo))
+    (set-fontset-font (frame-parameter nil 'font) charset
+                      (font-spec :family (car (cdr fonts))))))
+
+;; Always have cursor above a number of lines from bottom
 (setq scroll-margin 8)
-
-;(require 'package)
-;(add-to-list 'package-archives
-;  '("marmalade" . "http://marmalade-repo.org/packages/"))
-;(package-initialize)
