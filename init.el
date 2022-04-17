bb07375e941b7eb0c0ada2123c4f2a9e689835b4
Nevill <nevill.dutt@gmail.com>
2022-04-17 17:21:52 +0800

    A new start point

---
+++ init.el
@@ -0,0 +1,12 @@
+(global-set-key (kbd "s-a") 'mark-whole-buffer) ;;对应Windows上面的Ctrl-a 全选
+(global-set-key (kbd "s-c") 'kill-ring-save) ;;对应Windows上面的Ctrl-c 复制
+(global-set-key (kbd "s-s") 'save-buffer) ;; 对应Windows上面的Ctrl-s 保存
+(global-set-key (kbd "s-v") 'yank) ;对应Windows上面的Ctrl-v 粘贴
+(global-set-key (kbd "s-z") 'undo) ;对应Windows上面的Ctrol-z 撤销
+(global-set-key (kbd "s-x") 'kill-region) ;对应Windows上面的Ctrol-x 剪切
+
+(tool-bar-mode 0)
+; (global-linum-mode t) ;侧边显示行号
+(column-number-mode t) ;状态栏显示行列信息
+(show-paren-mode t) ;括号匹配高亮
+;(global-hl-line-mode 1) ;当前行高亮
